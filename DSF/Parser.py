3ff5619fd1496dc2f323f65456a2d9ead7d17bf6
Brandon Cuadrado <brancuad@gmail.com>
2017-11-12 16:45:21 -0500

    Bigrams appear as signle string, POS tagging for unigrams, sort by frequency and word, calculate frequency based on word/pos pair

--- DSF/Parser.py
+++ DSF/Parser.py
@@ -6,8 +6,14 @@ import sys
 import re
 import pandas as pd
 from pylatexenc.latex2text import LatexNodes2Text
+import numpy as np
+from pylatexenc.latexwalker import (
+    MacrosDef, LatexWalker, LatexToken, LatexCharsNode, LatexGroupNode, LatexCommentNode,
+    LatexMacroNode, LatexEnvironmentNode, LatexMathNode, LatexWalkerParseError
+)
 import nltk
 from TexSoup import TexSoup
+
 nltk.download('punkt', quiet=True)
 nltk.download('stopwords', quiet=True)
 
@@ -25,7 +31,13 @@ def checkWords(word):
     except:
         return False
 
-    if word in nltk.corpus.stopwords.words('english'):
+    if word.lower() in nltk.corpus.stopwords.words('english'):
+        return False
+
+    if word.isdigit():
+        return False
+
+    if len(word) < 2:
         return False
 
     pattern = re.compile("([A-Za-z0-9]+)")
@@ -37,7 +49,7 @@ def splitText(text):
     # delimiters - only for characters that separate words
     # conjoining characters not included
     splitChars = {'\.', ' ', '\,', ':', ';', '\n', '\*' \
-        '\|', '\?'}
+        '\|', '\?', '\(', '\)', '\^', '_'}
 
     splitString = ""
     for char in splitChars:
@@ -53,14 +65,18 @@ def splitText(text):
 
 
 # Create a dataset and preprocess its content
-def generateDataSet(words):
-    cols = ['words']
+def generateDataSet(words, type='unigram'):
+    cols = ['word']
     data_set = pd.DataFrame(columns=cols)
-    data_set['words'] = words
 
-    # Get frequency of each word
-    data_set['frequency'] = data_set.groupby('words')['words'].transform(pd.Series.value_counts)
-    data_set = data_set.drop_duplicates()
+    if (type is 'unigram'):
+        data_set['word'] = words
+
+    if (type is 'bigram'):
+        newWords = []
+        for bigram in words:
+            newWords.append(' '.join(bigram))
+        data_set['word'] = newWords
 
     return data_set
 
@@ -80,8 +96,12 @@ else:
 
     output_name = "CSV/text.csv"
 
+print('Reading LaTeX File...\n')
+
 # Get only the text from the file
-text = LatexNodes2Text().latex_to_text(file.read().encode('utf-8'))
+latextext = file.read().encode('utf-8')
+text = LatexNodes2Text().latex_to_text(latextext)
+lw = LatexWalker(latextext)
 
 ##############################################
 #   UNIGRAMS
@@ -100,18 +120,48 @@ df_final = data_set
 print('Gathering Bigrams...\n')
 bigrams = list(nltk.bigrams(words))
 
-dataset2 = generateDataSet(bigrams)
+dataset2 = generateDataSet(bigrams, type='bigram')
 
 df_final = df_final.append(dataset2, ignore_index = True)
 
 ##############################################
 #   POS Tagging
 ##############################################
+print('Determining Parts of Speech...\n')
+pos_tags = list(nltk.pos_tag(words))
+pos_df = pd.DataFrame(pos_tags, columns=['word', 'pos'])
+
+df_final = pd.concat([df_final, pos_df['pos']], axis=1)
+df_final['pos'] = df_final['pos'].replace(np.nan, 'NA')
+
+##############################################
+#   Frequencies
+##############################################
+print('Calculate Frequencies...\n')
+
+# Get frequency of each word
+df_final = df_final.groupby(['word', 'pos']).size().reset_index(name="frequency")
+
+##############################################
+#   Check Keywords
+##############################################
+
+# p = latextext.find(r'\kewords')
+# print(lw.get_token(pos=p))
+# exit()
+
+##############################################
+#   Check Formatting
+##############################################
 
 
 ##############################################
 #   Export CSV
 ##############################################
+
+# Sort by frequency
+df_final = df_final.sort_values(['frequency', 'word'], ascending = [False, True])
+
 print('Exporting CSV...\n')
 df_final.to_csv(output_name, index=False)
 
