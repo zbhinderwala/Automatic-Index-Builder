b29ec472bcf01619ce324bbbf5df45d32ebae5f8
Brandon Cuadrado <brancuad@gmail.com>
2017-12-05 16:46:35 -0500

    Move GoogleNgrams to separate file

---
+++ Source/Python/Tools/GoogleNgrams.py
@@ -0,0 +1,34 @@
+import phrasefinder
+import sys
+
+def GoogleNgrams(words):
+    match = []
+    volume = []
+
+    word_count = len(words)
+    counter = 1
+    for x in words:
+
+        sys.stdout.write("\r%d/%d" % (counter, word_count))
+        sys.stdout.flush()
+        match_str = '1'
+        vol_str = '1'
+
+        try:
+            # search for term x through Google Ngrams using phrasefinder
+            result = phrasefinder.search(x)
+
+            if result.status == phrasefinder.Status.Ok:   
+                if len(result.phrases) > 0:
+                    match_str = (result.phrases[0].match_count)
+                    vol_str = (result.phrases[0].volume_count)
+        except:
+            match_str = '-1'
+            vol_str = '-1'
+
+        match.append(match_str)
+        volume.append(vol_str)
+
+        counter += 1
+
+    return match, volume
